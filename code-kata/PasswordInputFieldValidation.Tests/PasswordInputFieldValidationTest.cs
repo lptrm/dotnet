1b6216231d8752a67c9b06909abb3c5ebac61332
jan <jobernberg@stud.hs-heilbronn.de>
2024-02-29 18:31:54 +0000

    refactoring

--- code-kata/PasswordInputFieldValidation.Tests/PasswordInputFieldValidationTest.cs
+++ code-kata/PasswordInputFieldValidation.Tests/PasswordInputFieldValidationTest.cs
@@ -3,15 +3,75 @@ namespace PasswordInputFieldValidation.Tests;
 public class PasswordInputFieldValidationTest
 {
     [Fact]
-    public void Given7CharString_whenValidatePassword_thenThrowsExceptionWithMessage()
+    public void Given7CharString_whenValidatePassword_thenIsNotValidAndHasErrorMessage()
     {
         //Given
         var validator = new PasswordInputFieldValidation();
-        var input = "Passwor";
+        var input = "Pa22wo!";
         //When
         validator = validator.ValidatePassword(input);
         //Then
         Assert.False(validator.IsValid);
         Assert.Equal("Password must be at least 8 characters", validator.ErrorMessage);
+    }
+    [Fact]
+    public void Given8CharStringWithoutNumbers_whenValidatePassword_thenIsNotValidAndHasErrorMessage()
+    {
+        // Given
+        var validator = new PasswordInputFieldValidation();
+        var input = "Passwor!";
+        // When
+        validator = validator.ValidatePassword(input);    
+        // Then
+        Assert.False(validator.IsValid);
+        Assert.Equal("Password must contain at least 2 numbers", validator.ErrorMessage);
+    }
+        [Fact]
+    public void Given7CharStringWithoutNumbers_whenValidatePassword_thenIsNotValidAndHasErrorMessage()
+    {
+        // Given
+        var validator = new PasswordInputFieldValidation();
+        var input = "Passwo!";
+        // When
+        validator = validator.ValidatePassword(input);    
+        // Then
+        Assert.False(validator.IsValid);
+        Assert.Equal("Password must be at least 8 characters\nPassword must contain at least 2 numbers", validator.ErrorMessage);
+    }
+    [Fact]
+        public void Given8CharStringWithoutCaps_whenValidatePassword_thenIsNotValidAndHasErrorMessage()
+    {
+        // Given
+        var validator = new PasswordInputFieldValidation();
+        var input = "pa22wor!";
+        // When
+        validator = validator.ValidatePassword(input);    
+        // Then
+        Assert.False(validator.IsValid);
+        Assert.Equal("Password must contain at least one capital letter", validator.ErrorMessage);
+    }
+    [Fact]
+    public void Given8CharStringWithoutSpecialChars_whenValidatePassword_thenIsNotValidAndHasErrorMessage()
+    {
+        // Given
+        var validator = new PasswordInputFieldValidation();
+        var input = "Pa22word";
+        // When
+        validator = validator.ValidatePassword(input);    
+        // Then
+        Assert.False(validator.IsValid);
+        Assert.Equal("Password must contain at least one special character", validator.ErrorMessage);
+    }
+    [Fact]
+    public void Given8CharStringWithSpecialCharsNumbersAndCaps_whenValidatePassword_thenIsValidAndErrorMessageIsEmpty()
+    {
+        // Given
+        var validator = new PasswordInputFieldValidation();
+        var input = "Pa22wor!";
+        // When
+        validator = validator.ValidatePassword(input);    
+        // Then
+        Assert.True(validator.IsValid);
+        Assert.True(string.IsNullOrEmpty(validator.ErrorMessage));
     }
 }
--- code-kata/PasswordInputFieldValidation/PasswordInputFieldValidation.cs
+++ code-kata/PasswordInputFieldValidation/PasswordInputFieldValidation.cs
@@ -1,21 +1,49 @@
-﻿using Microsoft.VisualBasic;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace PasswordInputFieldValidation;
 
-public class PasswordInputFieldValidation
-{   
-    public bool IsValid {get; private set;}
-    public string ErrorMessage {get; private set;}
-    public PasswordInputFieldValidation(){
-        IsValid = false;
+public partial class PasswordInputFieldValidation
+{
+    public bool IsValid { get; private set; }
+    public string ErrorMessage { get; private set; }
+    public PasswordInputFieldValidation()
+    {
+        IsValid = true;
         ErrorMessage = "";
     }
-    public PasswordInputFieldValidation ValidatePassword(string input){
-        IsValid = true;
-        if (input.Length < 8){
-            IsValid = false;
-            ErrorMessage = "Password must be at least 8 characters";
+    public PasswordInputFieldValidation ValidatePassword(string input)
+    {
+        var errorMessageBuilder = new StringBuilder();
+        if (input.Length < 8)
+        {
+            Invalidate("Password must be at least 8 characters", errorMessageBuilder);
         }
+        if (NumberRegex().Count(input) < 2)
+        {
+            Invalidate("Password must contain at least 2 numbers", errorMessageBuilder);
+        }
+        if (CapsRegex().Count(input) < 1)
+        {
+            Invalidate("Password must contain at least one capital letter", errorMessageBuilder);
+        }
+        if (SpecialCharsRegex().Count(input) < 1)
+        {
+            Invalidate("Password must contain at least one special character", errorMessageBuilder);
+        }
+        ErrorMessage = errorMessageBuilder.ToString();
         return this;
     }
+    private void Invalidate(string message, StringBuilder errorMessageBuilder)
+    {
+        if (IsValid) { IsValid = false; }
+        if (errorMessageBuilder.Length > 0) { errorMessageBuilder.AppendLine(); }
+        errorMessageBuilder.Append(message);
+    }
+    [GeneratedRegex(@"\d")]
+    private static partial Regex NumberRegex();
+    [GeneratedRegex("[A-Z]")]
+    private static partial Regex CapsRegex();
+    [GeneratedRegex(@"[!@#$%^&*()_+\-=\[\]{};':\\|,.<>\/?]")]
+    private static partial Regex SpecialCharsRegex();
 }
