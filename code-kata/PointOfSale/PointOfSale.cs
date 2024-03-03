6b62877eff7a3d824219191c0eb45fc5211b2e65
jan <jobernberg@stud.hs-heilbronn.de>
2024-03-03 14:22:19 +0000

    refactoring

--- code-kata/PointOfSale/PointOfSale.cs
+++ code-kata/PointOfSale/PointOfSale.cs
@@ -2,53 +2,68 @@
 
 public class PointOfSale
 {
-    public string Amount {get; private set;}
-    public PointOfSale(){
+    public string Amount { get; private set; }
+    public PointOfSale()
+    {
         Amount = "";
     }
-    public string Scan(string barCode){
+    public string Scan(string barCode)
+    {
         var intAmount = 0;
-        if(barCode.Contains("total\n")){
+        if (barCode.Contains("total\n"))
+        {
             var split = barCode.Split("\n");
             for (int i = 1; i < split.Length; i++)
             {
-                if(IsValidBarCode(split[i])){
+                if (IsValidBarCode(split[i]))
+                {
                     intAmount += GetIntFromBarCode(split[i]);
                 }
             }
-        } else {
-            if(IsValidBarCode(barCode)){
+        }
+        else
+        {
+            if (IsValidBarCode(barCode))
+            {
                 intAmount += GetIntFromBarCode(barCode);
             }
         }
-        if(!Amount.StartsWith("Error")){
+        if (!Amount.StartsWith("Error"))
+        {
             var stringRep = intAmount.ToString();
-            Amount = $"${stringRep[..(stringRep.Length-2)]}.{stringRep[(stringRep.Length-2)..]}";
+            Amount = $"${stringRep[..(stringRep.Length - 2)]}.{stringRep[(stringRep.Length - 2)..]}";
         }
-        
+
         return Amount;
     }
-    private bool IsValidBarCode(string barCode){
-        if (barCode.Equals("99999")){
-            if(Amount.StartsWith("Error")){
-                Amount += "\nError: barcode not found";
-            } else{
-                Amount = "Error: barcode not found";
-            }
+    private bool IsValidBarCode(string barCode)
+    {
+        if (barCode.Equals("99999"))
+        {
+            SetErrorMessage("Error: barcode not found");
             return false;
         }
-        if (string.IsNullOrEmpty(barCode)){
-            if(Amount.StartsWith("Error")){
-                Amount += "\nError: barcode not found";
-            } else {
-                Amount = "Error: empty barcode";
-            }
+        if (string.IsNullOrEmpty(barCode))
+        {
+            SetErrorMessage("Error: emtpy barcode");
             return false;
         }
-        return true ;
+        return true;
+    }
+    private void SetErrorMessage(string errorMessage)
+    {
+        if (Amount.StartsWith("Error"))
+        {
+            Amount += "\n" + errorMessage;
+        }
+        else
+        {
+            Amount = errorMessage;
+        }
     }
 
-    private static int GetIntFromBarCode(string barCode){
+    private static int GetIntFromBarCode(string barCode)
+    {
         return barCode switch
         {
             "12345" => 725,
